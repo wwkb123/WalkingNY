a572e461bc4cc4417e51d9a73e6120bd89a0f90c
wwkb123 <wwkb1233@gmail.com>
2018-07-05 16:07:26 -0400

    add responsive text size

--- app/src/main/java/edu/cuny/lagcc/laguardiawagnerarchive/WalkingNY/Email_Fragment.java
+++ app/src/main/java/edu/cuny/lagcc/laguardiawagnerarchive/WalkingNY/Email_Fragment.java
@@ -5,12 +5,14 @@ import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.TextViewCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class Email_Fragment extends Fragment implements FragmentLifecycle {
     private static final String TAG = "TAB3";
@@ -19,6 +21,29 @@ public class Email_Fragment extends Fragment implements FragmentLifecycle {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.email_fragment,container,false);
+
+        ////////////Responsive text size////////////////
+
+        TextView text1 = view.findViewById(R.id.emailTab_1);
+        TextViewCompat.setAutoSizeTextTypeWithDefaults(text1,TextViewCompat.AUTO_SIZE_TEXT_TYPE_UNIFORM);
+        TextViewCompat.setAutoSizeTextTypeUniformWithConfiguration( text1,  12, 24,2,2);
+
+        TextView text2 = view.findViewById(R.id.emailTab2);
+        TextViewCompat.setAutoSizeTextTypeWithDefaults(text2,TextViewCompat.AUTO_SIZE_TEXT_TYPE_UNIFORM);
+        TextViewCompat.setAutoSizeTextTypeUniformWithConfiguration( text2,  12, 24,2,2);
+
+        TextView text3 = view.findViewById(R.id.emailTab3);
+        TextViewCompat.setAutoSizeTextTypeWithDefaults(text3,TextViewCompat.AUTO_SIZE_TEXT_TYPE_UNIFORM);
+        TextViewCompat.setAutoSizeTextTypeUniformWithConfiguration( text3,  12, 24,2,2);
+
+        TextView text4 = view.findViewById(R.id.textView5);
+        TextViewCompat.setAutoSizeTextTypeWithDefaults(text4,TextViewCompat.AUTO_SIZE_TEXT_TYPE_UNIFORM);
+        TextViewCompat.setAutoSizeTextTypeUniformWithConfiguration( text4,  12, 24,2,2);
+
+
+        /////////////End of responsive text size/////////////
+
+
         Button composeEmailButton = view.findViewById(R.id.composeEmailButton);
         composeEmailButton.setTransformationMethod(null); //disable all caps text
 
