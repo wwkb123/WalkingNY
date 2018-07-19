49805e6da75ed92f13212f90a23ffd077328c96a
wwkb123 <wwkb1233@gmail.com>
2018-07-19 13:12:25 -0400

    remove debug screensize codes

--- app/src/main/java/edu/cuny/lagcc/laguardiawagnerarchive/WalkingNY/Home_Fragment.java
+++ app/src/main/java/edu/cuny/lagcc/laguardiawagnerarchive/WalkingNY/Home_Fragment.java
@@ -564,16 +564,6 @@ public class Home_Fragment extends Fragment implements FragmentLifecycle {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-
-                DisplayMetrics metrics = new DisplayMetrics();
-                getActivity().getWindowManager().getDefaultDisplay().getMetrics(metrics);
-
-                int height = metrics.heightPixels;
-                int width = metrics.widthPixels;
-
-                Log.e("height",height+"");
-                Log.e("width",width+"");
-
                final Bundle bundle = new Bundle();
                 bundle.putString("num_of_images", numberOfImages+"");
                 bundle.putString("arrayToPass", arrayToPass);
